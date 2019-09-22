20db1b1d69fcc2c0105a0eecfd4c409939baeaad
modderBUG <810237976@qq.com>
2019-09-23 00:01:53 +0800

    模拟事务锁，若失败把操作回滚。

--- handler/expaperhandler.js
+++ handler/expaperhandler.js
@@ -3,21 +3,29 @@ var fs = require('fs');
 var querystring = require('querystring');
 var path = require('path');
 var sqlhandler = require('../models/db/sqlhandler.js'); //
-var context=require('../config/main/jsonconfig.js');
+var context = require('../config/main/jsonconfig.js');
 
 /** 增加试卷，此业务需要执行以下几个操作！
  * 1.建立json对象
  * 2.把json写入磁盘
- * 3.把数据写入到数据库
+ * 3.把元数据写入到数据库
  * **/
 module.exports.addExpaper = function (req, res) {
 
-
+    /** 先在磁盘写json文件 然后把信息写入数据库 如果出错则回滚操作删除文件 （模拟事务锁）**/
     writeNewsDate(JSON.stringify(context.jsonobj(req)), function (content) {
-
-        sqlhandler.addSql(req,res,context.sqlobj(req,content));
-
-        //res.send(JSON.stringify(context.jsonobj(req)));
+        sqlhandler.addSql(req, res, context.sqlobj(req, content), function (err) {
+            if (err) {
+                fs.unlink(content, function (error) {
+                    if (error) {
+                        console.log(error);
+                        return false;
+                    }
+                    console.log('因为数据库写入错误，已写下文件删除成功！');
+                });
+                throw err;
+            }
+        });
     });
 
 
@@ -48,8 +56,8 @@ function readNewsData(callback) {
 //封装写入jsonvar
 function writeNewsDate(data, callback) {
     var dataPath = path.join(config.dataPath, "data1");  //获取data路径
-    var count=fs.readdirSync(dataPath).length+1
-    dataPath=path.join(dataPath,"expaper"+count+".json");
+    var count = fs.readdirSync(dataPath).length + 1
+    dataPath = path.join(dataPath, "expaper" + count + ".json");
 
     fs.writeFile(dataPath, data, function (err) {
         if (err) {
--- models/db/sqlhandler.js
+++ models/db/sqlhandler.js
@@ -1,13 +1,14 @@
 
 var Expaper = require('./expaper');
 
-module.exports.addSql=function (req,res,message) {
+module.exports.addSql=function (req,res,message,callback) {
 
     //如果没有post数据或者数据为空,直接返回
     if (message.name == undefined ||message.name == ''
         || message.content_path == undefined || message.content_path == '') {
         res.render('404', {});
-        return;
+        callback(new Error('执行sql语句出错！'));
+        return ;
     }
     //创建一条记录,创建成功后跳转回首页
     Expaper.create(message).then(function(msg){
