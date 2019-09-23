51b7f35f184003f723532ced13b577c391d557e1
Chen TIAN <ctian@futurewei.com>
2019-09-23 11:33:18 -0700

    update

--- config/main/config.js
+++
@@ -1,6 +0,0 @@
-var path=require('path');
-module.exports={
-    "port":3000,
-    "dataPath":path.join(__dirname,'../../public/data'),
-    "dataPathDir":path.join(__dirname,'../../public/data')
-};
